3a26ac9b9274e9288f4e04d173df6bec66f75952
SzymonGo1337 <46822941+SzymonGo1337@users.noreply.github.com>
2021-11-14 14:56:33 +0100

    mat4x4 some changes

--- src/Hukan/utils/Mat4x4.hpp
+++ src/Hukan/utils/Mat4x4.hpp
@@ -3,6 +3,18 @@
 #include "../Hukan.hpp"
 
 namespace hk {
+    constexpr Int64_t pi = 3.14159265359;
+
+    template<typename T>
+    constexpr T radians(T degrees) {
+        return degrees / 180.0f * static_cast<T>(pi);
+    }
+
+    template<typename T>
+    constexpr T degrees(T radians) {
+        return radians * 180.0f / static_cast<T>(pi);
+    }
+
     template<typename T>
     struct Mat4x4 {
     public:
@@ -11,10 +23,11 @@ namespace hk {
 
         Mat4x4() { Clear(); }
 
-        Mat4x4(T halfFov, T aspectRatio, T zNear, T zFar) {
+        Mat4x4(T fov, T aspectRatio, T zNear, T zFar) { // REMEMBER FOV IN RADIANS
             Clear();
-            matrix[0][0] = 1.0f / aspectRatio * halfFov;
-            matrix[1][1] = 1.0f / halfFov;
+            T _halfFov = 1.0f / std::tan(1.0f * fov);
+            matrix[0][0] = 1.0f / aspectRatio * _halfFov;
+            matrix[1][1] = 1.0f / _halfFov;
             matrix[2][2] = zFar / (zFar - zNear);
             matrix[2][3] = 1.0f;
             matrix[3][2] = -(2.0f * zFar * zNear) / (zFar - zNear);
@@ -53,6 +66,20 @@ namespace hk {
             matrix[x][y] = data;
         }
 
+        Mat4x4<T> operator*(const Mat4x4<T>& mat) {
+            Mat4x4<T> _result;
+
+            for(auto x = 0; x < 4; ++x) {
+                for(auto x = 0; x < 4; ++x) {
+                    for(auto x = 0; x < 4; ++x) {
+                        _result.matrix[x][y] += mat.matrix[x][z] * matrix[z][y];
+                    }
+                }
+            }
+
+            return _result;
+        }
+
         HK_NODISCARD T* GetMatrixAt(Int_t x, Int_t y) {
             return &matrix[x][y];
         }
@@ -63,4 +90,31 @@ namespace hk {
     using Mat4x4u = Mat4x4<Uint_t>;
     using Mat4x4l = Mat4x4<Long_t>;
     using Mat4x4d = Mat4x4<Double_t>;
+
+    template<typename T>
+    constexpr Mat4x4<T> Projection(T fov, T aspectRatio, T zNear, T zFar) {
+        return Mat4x4<T>(fov, aspectRatio, zNear, zFar);
+    }
+
+    template<typename T>
+    constexpr Mat4x4<T> Ortho2D(T left, T right, T bottom, T top, T zNear, T zFar) {
+        return Mat4x4<T>(left, right, bottom, top, zNear, zFar);
+    }
+
+    template<typename T>
+    constexpr void Translate(Mat4x4<T>& mat, const Vec3<T>& vec) {
+        mat.matrix[3][0] = mat.matrix[0][0] * vec.x + mat.matrix[1][0] * vec.y + mat.matrix[2][0] * vec.z + mat.matrix[3][0];
+	    mat.matrix[3][1] = mat.matrix[0][1] * vec.x + mat.matrix[1][1] * vec.y + mat.matrix[2][1] * vec.z + mat.matrix[3][1];
+        mat.matrix[3][2] = mat.matrix[0][2] * vec.x + mat.matrix[1][2] * vec.y + mat.matrix[2][2] * vec.z + mat.matrix[3][2];
+	    mat.matrix[3][3] = mat.matrix[0][3] * vec.x + mat.matrix[1][3] * vec.y + mat.matrix[2][3] * vec.z + mat.matrix[3][3];
+    }
+
+    template<typename T>
+    constexpr void Scale(Mat4x4<T>& mat, const Vec3<T>& vec) {
+        Mat4x4<T> _result;
+        mat.matrix[0][0] = vec.x;
+        mat.matrix[1][1] = vec.y;
+        mat.matrix[2][2] = vec.z;
+        mat = mat * _result;
+    }
 };
